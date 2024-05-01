cc6183d7698fe486838a9eca1de63a4ebe31b462
Mayank <mayank1365@gmail.com>
2024-05-01 12:23:05 +0530

    Initial commit

---
+++ src/main/java/com/mayank/vehicle_service_management_project_backend/VehicleServiceManagementProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.vehicle_service_management_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceManagementProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceManagementProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayank/vehicle_service_management_project_backend/VehicleServiceManagementProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.vehicle_service_management_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceManagementProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
