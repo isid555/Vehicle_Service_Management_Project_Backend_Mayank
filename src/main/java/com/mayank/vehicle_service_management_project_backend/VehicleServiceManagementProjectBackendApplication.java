9f1d8db2a8260a75242c4b795fc7d6f984c8f12b
isid555 <r555sid@gmail.com>
2024-05-01 12:47:10 +0530

    Sid_Checking_1

--- src/main/java/com/mayank/vehicle_service_management_project_backend/VehicleServiceManagementProjectBackendApplication.java
+++ src/main/java/com/mayank/vehicle_service_management_project_backend/VehicleServiceManagementProjectBackendApplication.java
@@ -8,6 +8,7 @@ public class VehicleServiceManagementProjectBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceManagementProjectBackendApplication.class, args);
+		System.out.println("Siddharth : Checking 0505");
 	}
 
 }
